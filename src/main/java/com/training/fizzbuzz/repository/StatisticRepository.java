dd3bd50600cfd7c65addb9aa25266aa869a720da
Steve Alves-Blyt <steve.alves@protonmail.com>
2023-04-27 16:31:41 +0200

    feat(db): query + fix docker

--- src/main/java/com/training/fizzbuzz/repository/StatisticRepository.java
+++ src/main/java/com/training/fizzbuzz/repository/StatisticRepository.java
@@ -2,9 +2,11 @@ package com.training.fizzbuzz.repository;
 
 import com.training.fizzbuzz.model.StatisticEndpoint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface StatisticRepository extends JpaRepository<StatisticEndpoint, Long> {
+    @Query("SELECT MAX(count),parameters FROM StatisticEndpoint")
     public Optional<StatisticEndpoint> findFirstByOrderByCountDesc();
 }
